3ae1e40b9102a86803412104751ed1a32251676e
Stefan Lang <Stefan.Lang@med.lu.se>
2016-09-16 13:51:04 +0200

    Fixed a bug with the install.pl script regarding the starman init file.

--- SCExV/script/install.pl
+++ SCExV/script/install.pl
@@ -219,7 +219,7 @@ unless ( -d $install_path ) {
 }
 system ( "sed -e's!plugin_path!/$install_path!' $plugin_path/../htpcra.psgi >$install_path/htpcra.psgi ");
 my $patcher3 = stefans_libs::install_helper::Patcher->new($plugin_path."/../SCExV.starman.initd" );
-$patcher3->replace_string( "my \\\$app_home = '.*\\n", "my \$app_home = '$install_path'\n" );
+$patcher3->replace_string( "my \\\$app_home = '.*\\n", "my \$app_home = '$install_path';\n" );
 $patcher3->{'filename'} = "$install_path/SCExV.starman.initd";
 $patcher3-> write_file();
 
